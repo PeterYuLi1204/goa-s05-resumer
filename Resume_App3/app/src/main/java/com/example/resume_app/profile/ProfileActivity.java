266a3291943e405a7ec583b8eb86c0bc7f225c1b
Peter <peteryuli1204@gmail.com>
2022-04-18 23:26:14 -0700

    Added titles and buttons to the profile info tab

--- Resume_App3/app/src/main/java/com/example/resume_app/profile/ProfileActivity.java
+++ Resume_App3/app/src/main/java/com/example/resume_app/profile/ProfileActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.resume_app.R;
