a031bab260fe09324fe5506af7b1c27fd51ec50f
Leticia Cid <leticiacid0478@gmail.com>
2022-04-21 19:36:09 -0700

    Figuring stuff

--- Resume_App3/app/src/main/java/com/example/resume_app/resume_builder/ResumeData.java
+++ Resume_App3/app/src/main/java/com/example/resume_app/resume_builder/ResumeData.java
@@ -6,6 +6,7 @@ import com.example.resume_app.resume_builder.resume_editor.Education;
 import com.example.resume_app.resume_builder.resume_editor.Experience;
 import com.example.resume_app.resume_builder.resume_editor.Skill;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -55,9 +56,31 @@ public class ResumeData implements Serializable {
 
     // UNTESTED AND PROBABLY BROKEN --arthur
 
-    void saveAs(String fileName) {
+//    void saveAs(String fileName) {
+//        try {
+//            FileOutputStream fileOut = new FileOutputStream("/resume_data/" + fileName + ".ser");
+//            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+//            out.writeObject(this);
+//            out.close();
+//            fileOut.close();
+//        } catch (Exception e) {
+//            // do nothing
+//        }
+//    }
+//
+//    static ResumeData loadFrom(String fileName) throws IOException, ClassNotFoundException {
+//        FileInputStream fileIn = new FileInputStream("/resume_data/" + fileName + ".ser");
+//        ObjectInputStream in = new ObjectInputStream(fileIn);
+//        ResumeData loaded = (ResumeData) in.readObject();
+//        in.close();
+//        fileIn.close();
+//        return loaded;
+//    }
+
+    void save() {
+        File ohGodWhatAmIDoing = new File("FileyThings");
         try {
-            FileOutputStream fileOut = new FileOutputStream("/resume_data/" + fileName + ".ser");
+            FileOutputStream fileOut = new FileOutputStream(ohGodWhatAmIDoing);
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
             out.writeObject(this);
             out.close();
